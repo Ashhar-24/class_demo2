da7f28c3ee35f1f4fa6126c3bbdef9981a9e8cc7
Aditi <gingerjabami@gmail.com>
2024-04-24 20:20:05 +0530

    just added a cp question

---
+++ marks.cpp
@@ -0,0 +1,19 @@
+
+
+#include <bits/stdc++.h>
+using namespace std;
+#define int long long int
+
+signed main() {
+	int t;
+cin >> t;
+while(t--){
+    int n;
+    cin >> n;
+    if(n>50)
+    cout << " bhai mast marks hain " << endl;
+    else
+    cout << "bhai reels band kr, thoda padhle" << endl;
+}
+return 0;
+}
